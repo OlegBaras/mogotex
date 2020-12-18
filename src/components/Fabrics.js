9fe55ced9b8450e6d4385fc122389a417e764a5b
Oleg Baras <tbaras@gmail.com>
2020-12-18 23:55:49 +0000

    refactoring

--- src/components/Fabrics.js
+++ src/components/Fabrics.js
@@ -1,12 +1,20 @@
 import React from "react";
 import WorkClothes from "./WorkClothes";
 import TableCloths from "./TableCloths";
-
+import { FormattedMessage } from "react-intl";
 function Fabrics() {
   return (
-    <div className="fabrics">
-      <WorkClothes />
-      <TableCloths />
+    <div>
+      <div className="fabrics">
+        <WorkClothes />
+        <TableCloths />
+      </div>
+      <div className="message">
+        <FormattedMessage id="Message" />
+        <span>
+          <a href="http://www.mogotex.com">www.mogotex.com</a>
+        </span>
+      </div>
     </div>
   );
 }
